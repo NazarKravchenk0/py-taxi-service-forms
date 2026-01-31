d8cc6feb958183788bdfacbec882d627af4d20dd
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-31 14:53:54 +0200

    check

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,27 +1,57 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import (
-    index,
-    CarListView,
+from taxi.views import (
+    CarCreateView,
+    CarDeleteView,
     CarDetailView,
-    DriverListView,
+    CarListView,
+    CarUpdateView,
     DriverDetailView,
+    DriverListView,
+    ManufacturerCreateView,
+    ManufacturerDeleteView,
     ManufacturerListView,
+    ManufacturerUpdateView,
+    index,
 )
+
+app_name = "taxi"
 
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
-]
+    path("accounts/login/", auth_views.LoginView.as_view(),
+         name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(),
+         name="logout"),
 
-app_name = "taxi"
+    path("manufacturers/", ManufacturerListView.as_view(),
+         name="manufacturer-list"),
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete",
+    ),
+
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(),
+         name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(),
+         name="car-delete"),
+
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
+         name="driver-detail"),
+]
--- taxi/views.py
+++ taxi/views.py
@@ -1,54 +1,111 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Prefetch
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-from .models import Driver, Car, Manufacturer
+from taxi.forms import CarForm, ManufacturerForm
+from taxi.models import Car, Driver, Manufacturer
 
 
 @login_required
 def index(request):
-    """View function for the home page of the site."""
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
 
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
 
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits + 1,
+        "num_visits": num_visits,
     }
+    return render(request, "taxi/index.html", context)
 
-    return render(request, "taxi/index.html", context=context)
 
-
-class ManufacturerListView(LoginRequiredMixin, generic.ListView):
+@method_decorator(login_required, name="dispatch")
+class ManufacturerListView(ListView):
     model = Manufacturer
-    context_object_name = "manufacturer_list"
-    template_name = "taxi/manufacturer_list.html"
+    queryset = Manufacturer.objects.all().order_by("name")
     paginate_by = 5
 
 
-class CarListView(LoginRequiredMixin, generic.ListView):
+@method_decorator(login_required, name="dispatch")
+class CarListView(ListView):
     model = Car
     paginate_by = 5
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer").all()
 
 
-class CarDetailView(LoginRequiredMixin, generic.DetailView):
+@method_decorator(login_required, name="dispatch")
+class CarDetailView(DetailView):
     model = Car
 
 
-class DriverListView(LoginRequiredMixin, generic.ListView):
+@method_decorator(login_required, name="dispatch")
+class DriverListView(ListView):
     model = Driver
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_user_id"] = self.request.user.id
+        return context
 
-class DriverDetailView(LoginRequiredMixin, generic.DetailView):
+
+@method_decorator(login_required, name="dispatch")
+class DriverDetailView(DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+    queryset = Driver.objects.prefetch_related(
+        Prefetch(
+            "cars",
+            queryset=Car.objects.select_related("manufacturer"),
+        )
+    )
+
+
+class CarCreateView(LoginRequiredMixin, CreateView):
+    model = Car
+    form_class = CarForm
+    success_url = reverse_lazy("taxi:car-list")
+
+
+class CarUpdateView(LoginRequiredMixin, UpdateView):
+    model = Car
+    form_class = CarForm
+
+    def get_success_url(self):
+        return reverse_lazy("taxi:car-detail", kwargs={"pk": self.object.pk})
+
+
+class CarDeleteView(LoginRequiredMixin, DeleteView):
+    model = Car
+    success_url = reverse_lazy("taxi:car-list")
+
+
+class ManufacturerCreateView(LoginRequiredMixin, CreateView):
+    model = Manufacturer
+    form_class = ManufacturerForm
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class ManufacturerUpdateView(LoginRequiredMixin, UpdateView):
+    model = Manufacturer
+    form_class = ManufacturerForm
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class ManufacturerDeleteView(LoginRequiredMixin, DeleteView):
+    model = Manufacturer
+    success_url = reverse_lazy("taxi:manufacturer-list")
